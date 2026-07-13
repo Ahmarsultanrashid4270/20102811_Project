b5c3c8d93838b8afcf3a8976c4af384b849324a6
Ahmarsultanrashid4270 <chahmarsf786@gmail.com>
2026-07-13 10:01:49 +0100

    Add Function name Addanimal

---
+++ script.js
@@ -0,0 +1,14 @@
+function addAnimal() {
+
+    let animal= { 
+        
+        animal_id: document.getElementById("animal_id").value,
+        name: document.getElementById("name").value,
+        species: document.getElementById("species").value,
+        breed: document.getElementById("breed").value,
+        age: document.getElementById("age").value,
+        gender: document.getElementById("gender").value,
+        status: document.getElementById("status").value
+
+    };
+
